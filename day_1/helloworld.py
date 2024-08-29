9bcdc1578f5115e914f64d3b226a41292ba27238
Jayson <jaacoulter@gmail.com>
2024-08-29 16:28:58 -0400

    creating day_1 folder and helloworld file

---
+++ day_1/helloworld.py
@@ -0,0 +1,29 @@
+#import os library to run windows command-line commands
+import os
+#print the current python version using the 'os' method
+print(os.system('python --version'))
+
+#print operands of '3' and '4'
+print(3 + 4) #addition
+print(3 - 4) #subtraction
+print(3 * 4) #multiplication
+print(3 % 4) #modulus
+print(3 / 4) #division
+print(3 ** 4) #exponential
+print(3 // 4) #floor division
+
+print("Test Name") #Print my name
+print("My Family Name") #Print my family name
+print("United States") #Print my Country
+print("I am enjoying 30 days of python") #Print the saying
+
+#print the type of all the below items
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh','Python','Finland']))
+print(type("Test name"))
+print(type("Family Name"))
+print(type({9.8, 3.14, 2.7}))
+print(type((9.8, 3.14, 2.7)))
