3a9ccc61402e156a34262dddcc42be10c023b8a7
Jayson <jaacoulter@gmail.com>
2024-08-29 17:06:13 -0400

    Created the final parts of the Day 1 Exercise

--- day_1/helloworld.py
+++ day_1/helloworld.py
@@ -1,5 +1,9 @@
 #import os library to run windows command-line commands
 import os
+
+#import math to utilize 
+import math
+
 #print the current python version using the 'os' method
 print(os.system('python --version'))
 
@@ -27,3 +31,24 @@ print(type("Test name"))
 print(type("Family Name"))
 print(type({9.8, 3.14, 2.7}))
 print(type((9.8, 3.14, 2.7)))
+
+print(type(3)) #print int
+print(type(9.8)) #print float
+print(type(7j - 3)) #print complex
+print(type("string")) #print string
+print(type(True)) #print boolean
+print(type(["list0", "list1", "list2"])) #print list
+print(type((9.8,4.3,2.6))) #print tuple
+print(type({'key0':'value0','key1':'value1','key2':'value2'}))
+
+#The Euclidean distance is essentially the length of the shortest path between two points, assuming you can only move in straight lines between dimensions.
+#find the Euclidean distance between (2,3) and (10,8)
+
+point1 = (2,3) #point 1
+point2 = (10,8) #point 2
+
+#use sqrt
+#The math.sqrt function in Python is part of the math module and is used to calculate the square root of a given number.
+
+distance = math.sqrt((point2[0] - point1[0])**2 + (point2[1]) - point1[1]**2)
+print(distance)
